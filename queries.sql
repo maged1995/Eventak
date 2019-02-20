537da536509b31f474b4b0c986a4d92870ebfed2
Maged A. Saad <magedsaadaziz@hotmail.com>
2019-02-20 10:36:46 +0200

    Added tables

--- queries.sql
+++ queries.sql
@@ -8,6 +8,12 @@
     primary key(id)
   );
 
+  create table RelStat( /*Relationship status*/
+    f1id int not null, f2id int not null, stat int not null,
+    FOREIGN KEY (f1id) REFERENCES users(id),
+    FOREIGN KEY (f2id) REFERENCES users(id)
+  );
+
   create table events(
     id int not null auto_increment, name varchar(50) not null,
     locLong varchar(10) not null, locLat varchar(10) not null,
@@ -18,8 +24,20 @@
     id int not null auto_increment, name varchar(15), primary key(id)
   );
 
+  create table usrPrefs( /* user connection to eventTypes */
+    uid int not null, etid int not null,
+    FOREIGN KEY (uid) REFERENCES users(id),
+    FOREIGN KEY (etid) REFERENCES eventTypes(id)
+  );
+
   create table event_type(
     event int not null, type int not null,
     FOREIGN KEY (event) REFERENCES events(id),
     FOREIGN KEY (type) REFERENCES eventTypes(id)
+  );
+
+  create table user_event(
+    uid int not null, eid int not null, stat int not null,
+    FOREIGN KEY (uid) REFERENCES users(id),
+    FOREIGN KEY (eid) REFERENCES events(id)
   );
