2b45adbcb013d281e1dfcfa0fc0be96668c14560
Maged A. Saad <magedsaadaziz@hotmail.com>
2019-02-23 12:19:32 +0200

    added links and login page

--- urls.py
+++ urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('Login/', views.index, name='Login'),
 ]
--- views.py
+++ views.py
@@ -4,6 +4,13 @@ from django.template import loader
 def index(request):
     template = loader.get_template('index.html')
     context = {
-        
+
+    }
+    return HttpResponse(template.render(context, request))
+
+def index(request):
+    template = loader.get_template('login.html')
+    context = {
+
     }
     return HttpResponse(template.render(context, request))
