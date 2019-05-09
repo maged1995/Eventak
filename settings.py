25e3c5fda423a74c10cdf1c414bbf9527a2ca607
root <root@eventak2-xqk3.us-east1-b.c.glossy-champion-235111.internal>
2019-05-09 11:28:24 +0000

    updates in maps API

--- settings.py
+++ settings.py
@@ -141,3 +141,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+SESSION_COOKIE_SECURE=True
+SESSION_COOKIE_HTTPONLY=True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
